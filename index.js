90bca8a943db2d34c01f529c5ab9e8d1167e496a
Ian <ian.odhiambo@student.moringaschool.com>
2023-06-15 11:03:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31 
+let num2 = 2 
+let multiply = num1 * num2 
+
+let random = (Math.cell(Math.random()*10)+1)
+
+let num3 = 17
+let num4 = 13 
+let mod = num3 % num4
+
+let max =(Math.max(15,12,14,4,8,20,17,18))
